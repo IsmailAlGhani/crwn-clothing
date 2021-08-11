e892bf49fb2bc9385ec17e5baed740a737454a5a
Ismail Al Ghani <ismailalghani@gmail.com>
2021-08-11 14:05:06 +0700

    refactor styled components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,11 +3,10 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
-import { selectCartHidden } from '../../redux/cart/cart.selector';
-import { selectCurrentUser } from '../../redux/user/user.selector';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
-import './header.styles.scss';
 import { HeaderContainer, LogoContainer, OptionLink, OptionsContainer } from './header.styles';
 
 const Header = ({ currentUser, hidden }) => (
